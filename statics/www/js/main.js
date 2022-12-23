10633f8af1f72e6dcd8c5fb9e17b21e492f52627
fulldump <gerardooscarjt@gmail.com>
2022-12-23 01:22:30 +0100

    feat: publish in beta

--- statics/www/js/main.js
+++ statics/www/js/main.js
@@ -75,6 +75,8 @@ document.querySelectorAll("article.tweet").forEach(item => {
 
 let user = {};
 let XAuthHeader = '';
+const avatar = document.createElement('img');
+
 fetch('/auth/me').then(resp => {
     if (resp.status != 200) {
         throw 'bad status code';
@@ -96,6 +98,8 @@ fetch('/auth/me').then(resp => {
     picture.setAttribute('src', resp.picture);
     authDiv.append(picture);
 
+    avatar.setAttribute('src', resp.picture);
+
     let nick = document.createElement('span');
     nick.classList.add('auth-nick');
     nick.textContent = resp.nick;
@@ -112,3 +116,88 @@ fetch('/auth/me').then(resp => {
     login.href = '/auth/login';
     id('auth').append(login);
 });
+
+function sendHonk(f, message, parentHonkId) {
+    let body = {
+        message: message,
+    };
+    if (parentHonkId) body.parent_honk_id = parentHonkId;
+
+    fetch('/v0/publish', {method:'POST', body:JSON.stringify(body), headers: {'X-Glue-Authentication':XAuthHeader}})
+    .then(resp => resp.json())
+    .then(entry => {
+        // todo: check status code, network error
+        if (entry.error) {
+            alert(entry.error.message);
+            return
+        }
+        f(entry);
+    })
+    .catch(function (err) {
+        alert(err);
+    });
+}
+
+
+(function() {
+
+
+    let publish_dom = document.querySelector("#publish");
+
+    // avatar is created in global scope
+    avatar.classList.add('avatar');
+    avatar.src = user.picture;
+    publish_dom.appendChild(avatar);
+
+    const text_input = document.createElement('textarea');
+    text_input.setAttribute('rows', 4);
+    text_input.placeholder = '¿Qué está pasando?';
+    publish_dom.appendChild(text_input);
+
+    const buttons = document.createElement('div');
+    buttons.classList.add('buttons');
+    publish_dom.appendChild(buttons);
+
+    const MAX_LENGTH = 300;
+
+    const counter = document.createElement('span');
+    counter.classList.add('counter');
+    buttons.appendChild(counter);
+
+    const publish_button = document.createElement('button');
+    publish_button.textContent = "Honk!";
+    publish_button.setAttribute('disabled', true);
+    buttons.appendChild(publish_button);
+
+    update_counter = function() {
+        if (text_input.value.length == 0) {
+            publish_button.setAttribute('disabled', true);
+            counter.textContent = '';
+        } else {
+            publish_button.removeAttribute('disabled');
+            const remaining = MAX_LENGTH-text_input.value.length;
+            counter.textContent = ''+(remaining);
+            if (remaining >= 0) {
+                counter.classList.remove('red');
+                publish_button.removeAttribute('disabled');
+            } else {
+                counter.classList.add('red');
+                publish_button.setAttribute('disabled', true);
+            }
+        }
+    };
+
+
+    text_input.addEventListener('keyup', update_counter, true);
+    publish_button.addEventListener('click', function() {
+        sendHonk(function (honk) {
+            console.log({honk});
+            text_input.value = '';
+            update_counter();
+            location.reload();
+        }, text_input.value);
+    }, true);
+
+
+})();
+
